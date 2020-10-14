87347c227fd532442eb2eae50730f1ae57f80e67
Mika Vilpas <mika.vilpas@qvantel.com>
2020-10-14 17:11:34 +0300

    feat: build some files with minification and some without

--- webpack/base.js
+++ webpack/base.js
@@ -1,49 +1,76 @@
 const webpack                 = require("webpack");
 const path                    = require("path");
-const HtmlWebpackPlugin       = require("html-webpack-plugin");
-const { CleanWebpackPlugin }  = require("clean-webpack-plugin");
 const MiniCssExtractPlugin    = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin            = require("terser-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
-  entry: {
-    index: "./src/index.js",
-    darkTheme: "./src/styles/darkTheme.css",
-    externalLinksAsNewTabs: "./src/site-scripts/externalLinksAsNewTabs.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: "file-loader"
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+const moduleDefinitions = {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader"
       }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../")
-    }),
-    new MiniCssExtractPlugin(),
-    new OptimizeCssAssetsPlugin({
-      // assetNameRegExp: /\.optimize\.css$/g,
-      cssProcessor: require('cssnano'),
-      cssProcessorPluginOptions: {
-        preset: ['default', { discardComments: { removeAll: true } }],
-      },
-      canPrint: true
-    })
+    },
+    {
+      test: /\.(gif|png|jpe?g|svg|xml)$/i,
+      use: "file-loader"
+    },
+    {
+      test: /\.css$/i,
+      use: [MiniCssExtractPlugin.loader, 'css-loader'],
+    }
   ]
 };
+
+const plugins = [
+  new MiniCssExtractPlugin(),
+  new OptimizeCssAssetsPlugin({
+    // assetNameRegExp: /\.optimize\.css$/g,
+    cssProcessor: require('cssnano'),
+    cssProcessorPluginOptions: {
+      preset: ['default', { discardComments: { removeAll: true } }],
+    },
+    canPrint: true
+  })
+];
+
+module.exports = [
+  {
+    name: "devsite",
+    mode: "development",
+    devtool: "eval-source-map",
+    entry: {
+      index: "./src/index.js"
+    },
+    module: moduleDefinitions,
+    plugins: plugins
+  },
+  {
+    name: "scriptfiles",
+    mode: "production",
+    devtool: false,
+    entry: {
+      darkTheme: "./src/styles/darkTheme.css",
+      externalLinksAsNewTabs: "./src/site-scripts/externalLinksAsNewTabs.js"
+    },
+    module: moduleDefinitions,
+    plugins: plugins,
+    performance: {
+      maxEntrypointSize: 900000,
+      maxAssetSize: 900000
+    },
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            output: {
+              comments: false
+            },
+            cache: false
+          }
+        })
+      ]
+    }
+  }];
