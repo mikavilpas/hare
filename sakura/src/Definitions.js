41f082807e9b74ce728a6cf583b7e81d14329508
Mika Vilpas <mika.vilpas@qvantel.com>
2021-05-16 08:47:06 +0300

    feat: can render superscript and keyword elements in defs

--- sakura/src/Definitions.js
+++ sakura/src/Definitions.js
@@ -8,17 +8,44 @@ import React, { useState, useEffect } from "react";
 import { getDicts } from "./api";
 import config from "./config";
 
-const Definitions = ({ adict, searchResult }) => {
+import BBCode from "@bbob/react/es/Component";
+import reactPreset from "@bbob/preset-react/es";
+
+const mypreset = reactPreset.extend((tags, options) => ({
+  ...tags,
+  keyword: (node) => ({
+    tag: "mark",
+    content: node.content,
+  }),
+  superscript: (node) => ({
+    tag: "sup",
+    content: node.content,
+  }),
+}));
+
+function bbcode2React(text) {
+  console.log(text);
+  return (
+    <BBCode
+      plugins={[mypreset()]}
+      options={{ onlyAllowTags: ["i", "keyword", "superscript"] }}
+    >
+      {text}
+    </BBCode>
+  );
+}
+
+const Definitions = ({ dict, searchResult }) => {
   return (
     <Accordion className="definition-listing" defaultActiveKey="0">
       {searchResult?.words?.map((w, i) => {
         return (
           <Card key={i}>
             <Accordion.Toggle as={Card.Header} eventKey={i.toString()}>
-              <h2>{w?.heading}</h2>
+              <h4>{w?.heading}</h4>
             </Accordion.Toggle>
             <Accordion.Collapse eventKey={i.toString()}>
-              <Card.Body>{w?.text}</Card.Body>
+              <Card.Body>{bbcode2React(w.text)}</Card.Body>
             </Accordion.Collapse>
           </Card>
         );
