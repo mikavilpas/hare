5ac803ab8a7ecaeb4f9e6420c0a2db2e4cfa12e8
Mika Vilpas <mika.vilpas@qvantel.com>
2021-05-19 18:02:15 +0300

    feat: recursive search calls api for definitions

--- sakura/src/views/recursiveLookup/index.js
+++ sakura/src/views/recursiveLookup/index.js
@@ -2,17 +2,61 @@ import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import { useHistory, useParams } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
+import Alert from "react-bootstrap/Alert";
+
+import { getWordDefinitions } from "../../api";
 
 function RecursiveLookup({ word, hide }) {
+  const [searchResult, setSearchResult] = useState();
+  const [searchResultLoading, setSearchResultLoading] = useState(false);
+  const [searchResultError, setSearchResultError] = useState();
+
+  const dictname = "広辞苑";
+
+  useEffect(() => {
+    setSearchResult(null);
+    setSearchResultError(null);
+    setSearchResultLoading(true);
+    getWordDefinitions({ dict: dictname, word: word })
+      .then(([result, error]) => {
+        setSearchResult(result);
+        setSearchResultError(error);
+      })
+      .finally(() => setSearchResultLoading(false));
+  }, [word]);
+
+  const contents = () => {
+    if (searchResultLoading) {
+      return (
+        <div>
+          {word}
+          <Spinner animation="border" role="status">
+            <span className="sr-only">Loading...</span>
+          </Spinner>
+        </div>
+      );
+    }
+    if (searchResultError) {
+      return (
+        <Alert variant={"danger"}>
+          <p>Error loading results</p>
+          <p>{searchResultError.toString()}</p>
+        </Alert>
+      );
+    }
+
+    return word + " search successful";
+  };
+
   return (
     <Modal
-      show={word?.length}
+      show={word?.length > 0}
       onHide={() => hide()}
       dialogClassName="recursive-lookup"
     >
-      <Modal.Body>
-        <p>you looked up {word}</p>
-      </Modal.Body>
+      <Modal.Body>{contents()}</Modal.Body>
     </Modal>
   );
 }
