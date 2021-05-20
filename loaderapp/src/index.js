6d1a71ac80ce42ff77893076704c8acecef075f1
Mika Vilpas <mika.vilpas@qvantel.com>
2021-05-20 17:36:09 +0300

    feat: loaderapp provides prod and dev links for new site

--- loaderapp/src/index.js
+++ loaderapp/src/index.js
@@ -44,27 +44,58 @@ window.onload = async () => {
   // production import
   const base = "https://sp3ctum.github.io/sakura-paris-customizations";
 
-  displaySourceFiles({
-    sourceText: Mustache.render(
-      document.getElementById("prod-import-template").innerHTML,
-      { jsFile: `${base}/prod.js`, cssFile: `${base}/prod.css` }
-    ),
-    codeElement: document.getElementById("customizations-js"),
-    copyButtonElement: document.getElementById("copy-js-customizations-button"),
-    highlight: true,
-  });
+  async function productionImport() {
+    displaySourceFiles({
+      sourceText: Mustache.render(
+        document.getElementById("prod-import-template").innerHTML,
+        {
+          jsFile: `${base}/prod.js`,
+          cssFile: `${base}/prod.css`,
+          newUiJsFile: `${base}/new-ui/main.js`,
+          newUiCssFile: `${base}/new-ui/main.css`,
+        }
+      ),
+      codeElement: document.getElementById("customizations-js"),
+      copyButtonElement: document.getElementById(
+        "copy-js-customizations-button"
+      ),
+      highlight: true,
+    });
+  }
 
-  // dev import
-  const cssText = await concatFiles(["prod.css"]);
-  const jsText = await concatFiles(["prod.js"]);
+  async function devImport() {
+    // dev import
+    const cssText = await concatFiles(["prod.css"]);
+    const jsText = await concatFiles(["prod.js"]);
 
-  displaySourceFiles({
-    sourceText: Mustache.render(
-      document.getElementById("dev-import-template").innerHTML,
-      { css: JSON.stringify(cssText), js: JSON.stringify(jsText) }
-    ),
-    codeElement: document.getElementById("dev-import"),
-    copyButtonElement: document.getElementById("copy-dev-button"),
-    highlight: true,
-  });
+    displaySourceFiles({
+      sourceText: Mustache.render(
+        document.getElementById("dev-import-template").innerHTML,
+        { css: JSON.stringify(cssText), js: JSON.stringify(jsText) }
+      ),
+      codeElement: document.getElementById("dev-import"),
+      copyButtonElement: document.getElementById("copy-dev-button"),
+      highlight: true,
+    });
+  }
+
+  async function newUiDevImport() {
+    // dev import
+    const cssText = await concatFiles(["new-ui/main.css"]);
+    const jsText = await concatFiles(["new-ui/main.js"]);
+
+    displaySourceFiles({
+      sourceText: Mustache.render(
+        document.getElementById("new-ui-dev-import-template").innerHTML,
+        { css: JSON.stringify(cssText), js: JSON.stringify(jsText) }
+      ),
+      codeElement: document.getElementById("new-ui-dev-import"),
+      copyButtonElement: document.getElementById("copy-new-ui-dev-button"),
+      highlight: true,
+    });
+  }
+
+  productionImport();
+  devImport();
+  newUiDevImport();
 };
