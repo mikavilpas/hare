0992701e80a378e334cf36ce366d81ba43a81ba8
Mika Vilpas <mika.vilpas@qvantel.com>
2021-05-20 17:23:14 +0300

    build: copy new ui files to release folder

--- loaderapp/webpack/base.js
+++ loaderapp/webpack/base.js
@@ -3,6 +3,7 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const moduleDefinitions = {
   rules: [
@@ -33,6 +34,15 @@ const plugins = [
       preset: ["default", { discardComments: { removeAll: true } }],
     },
     canPrint: true,
+  }),
+  new CopyPlugin({
+    patterns: [
+      { from: "../sakura/build/static/js/main.js", to: "new-ui/" },
+      { from: "../sakura/build/static/js/main.js.map", to: "new-ui/" },
+      { from: "../sakura/build/static/js/main.js.LICENSE.txt", to: "new-ui/" },
+      { from: "../sakura/build/static/css/main.css", to: "new-ui/" },
+      { from: "../sakura/build/static/css/main.css.map", to: "new-ui/" },
+    ],
   }),
 ];
 
