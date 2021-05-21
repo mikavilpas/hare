f71ae6c1ea8fe9a265206dec94f9a210a13a5234
Mika Vilpas <mika.vilpas@qvantel.com>
2021-05-21 16:09:06 +0300

    other: add test todo

--- sakura/cypress/integration/DictionarySpec.js
+++ sakura/cypress/integration/DictionarySpec.js
@@ -107,3 +107,4 @@ describe("dictionary view", () => {
 });
 
 // TODO can't render the first definition's last line properly http://localhost:4000/dict/%E5%BA%83%E8%BE%9E%E8%8B%91/prefix/%E7%A7%8B
+// TODO no results for recursive search /dict/広辞苑/prefix/犬/recursive/大辞林/prefix/呼ぶ
