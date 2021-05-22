56747fa0350903b551d4c82a4c60bd11fb8599f4
Mika Vilpas <mika.vilpas@qvantel.com>
2021-05-22 22:02:10 +0300

    fix: custom loading

--- loaderapp/src/installToHostSite.js
+++ loaderapp/src/installToHostSite.js
@@ -2,7 +2,6 @@ import { loadJs, base } from "./loader/utils";
 
 // this file is the piece of code that the user needs to copy to the host app's
 // configuration in order to get the app running.
-loadJs(`${base}/loader.js`);
-
-// now that loader.js has been loaded, we can use its functions
-window.load();
+loadJs(`${base}/loader.js`).then(() => {
+  window.load();
+});
