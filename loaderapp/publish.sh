757708f3f4f131ed950394ce129f75859899c911
Mika Vilpas <mika.vilpas@qvantel.com>
2021-05-22 16:12:35 +0300

    secret test release for the new ui v2

--- loaderapp/publish.sh
+++ loaderapp/publish.sh
@@ -3,9 +3,10 @@ set -e
 set +x
 
 rm ./dist/* -r
+mkdir -p ../docs
 
 (cd ../sakura/ && yarn build)
 yarn build
-cp -v ./dist/* docs/ -r
-git add docs
+cp -v ./dist/* ../docs/ -r
+git add ../docs
 git status
