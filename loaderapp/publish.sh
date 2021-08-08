5230b2588fec1a29618c2b53cb7a742843c016f9
Mika Vilpas <mika.vilpas@qvantel.com>
2021-08-08 19:08:57 +0300

    fix: build with correct public url

--- loaderapp/publish.sh
+++ loaderapp/publish.sh
@@ -5,6 +5,7 @@ set +x
 rm ./dist/* -rf
 mkdir -p ../docs
 
+export PUBLIC_URL=https://sp3ctum.github.io/hare/
 (cd ../sakura/ && yarn prettier-check && yarn build)
 yarn build
 cp -v ./dist/* ../docs/ -r
